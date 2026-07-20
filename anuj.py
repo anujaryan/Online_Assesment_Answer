8c3061a3a3f32a570427b38345658fde7bf58834
Anuj Kumar <156888339+anujaryan@users.noreply.github.com>
2026-07-20 17:46:56 +0530

    Add files via upload
    Answer

---
+++ anuj.py
@@ -0,0 +1,19 @@
+def longest_Valid_Parentheses(s):
+    stack = [-1]
+    ans = 0
+
+    for i, ch in enumerate(s):
+        if ch == "(":
+            stack.append(i)
+        else:
+            stack.pop()
+
+            if not stack:
+                stack.append(i)
+            else:
+                ans = max(ans, i - stack[-1])
+
+    return ans
+
+
+print(longest_Valid_Parentheses(input()))
